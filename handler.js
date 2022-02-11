d433b501c8f0a597e4a2aecfeccc5487852afb0b
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-11 05:22:14 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -455,12 +455,12 @@ ${(global.linkGC).map((v, i) => '*Group ' + (i + 1) + '*\n' + v).join`\n\n`}
               text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Hola 😙, *@user*\n───────────────────\n*🎊Bienvenido/a al grupo* :\n@subject\n\n• *Descripción* :\n\n@desc').replace('@subject', await this.getName(jid)).replace('@desc', groupMetadata.desc) :
                 (chat.sBye || this.bye || conn.bye || '@user un gay salio del grupo')).replace('@user', '@' + user.split('@')[0])
               addz = "```– anu.action == 'add' –```"
-              py = await this.sendMessage(jid, await(await fetch(pp)).buffer(), MessageType.image, {contextInfo: { externalAdReply:{title: "ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ",body:"", previewType:"PHOTO",thumbnail: await(await fetch(pp)).buffer(), sourceUrl:`https://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw`}}})
+              let pyy = await this.sendMessage(jid, await(await fetch(pp)).buffer(), MessageType.image, {contextInfo: { externalAdReply:{title: "ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ",body:"", previewType:"PHOTO",thumbnail: await(await fetch(pp)).buffer(), sourceUrl:`https://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw`}}})
 gbutsan = [
 {buttonId: 'qqq', buttonText: {displayText: `${(action === 'add' ? 'Gracias 🧃' : 'Adios 👋', 'Lolibot - OFC')}`}, type: 1}
 ]
 gbuttonan = {
-imageMessage: py.message.imageMessage,
+imageMessage: pyy.message.imageMessage,
 contentText: text,
 footerText: `${addz}`,
 buttons: gbutsan,
