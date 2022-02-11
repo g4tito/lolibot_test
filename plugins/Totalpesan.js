8e6573d7baa3dd2687e1bf40bafe3ed331f7f551
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-11 11:14:23 -0500

    Add files via upload

--- plugins/Totalpesan.js
+++ plugins/Totalpesan.js
@@ -12,10 +12,10 @@ let handler = async (m, { conn }) => {
         totalM++
     }, 1000)
     let sorted = Object.entries(mCount).sort((a, b) => b[1] - a[1])
-    let pesan = sorted.map(v => `${v[0].replace(/(\d+)@.+/, '@$1')}: ${v[1]} pesan`).join('\n')
-    m.reply(`${totalM} pesan terakhir\n${pesan}`, false, { contextInfo: { mentionedJid: sorted.map(v => v[0]) } })
+    let pesan = sorted.map(v => `${v[0].replace(/(\d+)@.+/, '@$1')}: ${v[1]} mensajes`).join('\n')
+    m.reply(`*Total* :${totalM} mensajes\n\n${pesan}`, false, { contextInfo: { mentionedJid: sorted.map(v => v[0]) } })
 }
-handler.help = ['totalpesan']
+handler.help = ['totalmsg']
 handler.tags = ['group']
 
 handler.command = /^totalpesan$/i
