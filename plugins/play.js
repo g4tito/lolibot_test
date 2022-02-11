5cbadc59ac74fbcc53381486fff818cad07f24b0
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-10 19:49:48 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -15,10 +15,11 @@ let handler = async (m, { conn, command, text, isPrems, isOwner, DevMode }) => {
           let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
           let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
           conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*Título* : _${title}_
+*Tamaño* : _${filesizeF}_
+*Link* : _${vid.url}_
+
+${isLimit ? 'Pakai ': ''}*Link de descarga* : _${dl_link}_
 `.trim(), m)
           if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
 *Title:* ${title}
