d9f6d32880f3ad101ab5f20113b6fd5d877506d3
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-10 22:57:59 -0500

    Add files via upload

--- plugins/daily.js
+++ plugins/daily.js
@@ -6,12 +6,12 @@ let handler = async (m, { conn }) => {
     let _timers = (cooldown - __timers)
     let timers = clockString(_timers)
     if (new Date - user.lastclaim > cooldown) {
-        conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan 1000 💵money dan 1 potion`, m)
+        conn.reply(m.chat, `Has reclamado 1000 de dinero y una poci�n`, m)
         global.DATABASE._data.users[m.sender].money += 1000
         global.DATABASE._data.users[m.sender].potion += 1
         global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
     } else {
-        let buttons = button(`silahkan tunggu *🕒${timers}* lagi untuk bisa mengclaim lagi`, user)
+        let buttons = button(` Espere *${timers}* para volver a reclamar`, user)
         conn.sendMessage(m.chat, buttons, MessageType.buttonsMessage, { quoted: m })
     }
 }
@@ -47,7 +47,7 @@ function button(teks, user) {
 
     const buttonMessage = {
         contentText: teks,
-        footerText: '©games-wabot',
+        footerText: 'Lolibot - OFC',
         buttons: buttons,
         headerType: 1
     }
