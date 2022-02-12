b78b06d416fc7d340f082bbb7acc1a0d0eaee52d
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-12 14:15:35 -0500

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -37,6 +37,14 @@ const defaultMenu = {
   before: `
 Hola %name, %ucapan!
 
+*៹࣪ 🧃₊∙ BOT INFO ₊°›››*
+
+• *Nombre* : _${conn.getName(conn.user.jid)}_
+• *Prefix* : _< Multiprefijo >_
+• *Navegador* : _${Fg.browserDescription[1]}_
+• *Servidor* : _${Fg.browserDescription[0]}_
+• *Version* : _${Fg.browserDescription[2]}_
+
 • *Fecha* : _%week %weton, %date_
 • *Fecha islamica* : _%dateIslamic_
 • *Hora* : _%time_
@@ -63,7 +71,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     // Offset -420 is 18.00
     // Offset    0 is  0.00
     // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let weton = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves','Viernes','Sábado'][Math.floor(d / 84600000) % 5]
     let week = d.toLocaleDateString(locale, { weekday: 'long' })
     let date = d.toLocaleDateString(locale, {
       day: 'numeric',
@@ -148,7 +156,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
      let tumb = await(await fetch("https://w.wallpaperkiss.com/wimg/s/151-1513066_small.jpg")).buffer()
      let tumbb = await(await fetch("https://i.ibb.co/YQxJTwx/long-hair-anime-girls-gochuumon-wa-usagi-desu-ka-kafuu-chino-wallpaper-preview.jpg")).buffer()
      conn.sendMessage(m.chat, { contentText: text.trim(), footerText: 'Lolibot - OFC', buttons: [{buttonId: '/ping', buttonText: {displayText: '👾 PING'}, type: 1},{buttonId: '/owner' , buttonText: {displayText: '🍧 CREADOR'}, type: 1}], "headerType": "DOCUMENT", "documentMessage": { "url": "https://mmg.whatsapp.net/d/f/Ano5cGYOFQnC51uJaqGBWiCrSJH1aDCi8-YPQMMb1N1y.enc", "mimetype": "application/vnd.ms-excel", "title": "Dibuat Oleh: Arifi Razzaq", "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=", "fileLength": 999999999999, "pageCount": 999999999, "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=", "fileName": "𝕷𝖔𝖑𝖎𝖇𝖔𝖙 - 𝕺𝖋𝖎𝖈𝖎𝖆𝖑™.⁖⃟•᭄", "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=", "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC", "mediaKeyTimestamp": "1634472176", "jpegThumbnail": tumb 
-            }}, MessageType.buttonsMessage, { quoted: m, thumbnail: tumbb, contextInfo: { externalAdReply: { title: `あなたは私のすべてです`, body: `Macielly ? D‵ Gatito`, thumbnail: tumbb, mediaType:"2", previewType: "VIDEO", mediaUrl: ""
+            }}, MessageType.buttonsMessage, { quoted: m, thumbnail: tumbb, contextInfo: { forwardingScore: 750, isForwarded: true, externalAdReply: { title: `あなたは私のすべてです`, body: `Macielly ? D‵ Gatito`, thumbnail: tumbb, mediaType:"2", previewType: "VIDEO", mediaUrl: ""
             }
             }
             })
