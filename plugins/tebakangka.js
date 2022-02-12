dd59934f14a4fe3a0baca7ac30da02ea577b3f3c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-12 00:58:14 -0500

    Add files via upload

--- plugins/tebakangka.js
+++ plugins/tebakangka.js
@@ -1,21 +1,22 @@
 let handler = async (m, { conn, args }) => {
     const bonus = Math.floor(Math.random() * 300)
-    if (!args[0]) throw 'Harap masukan pilihan angka yang akan kamu pilih'
-    if (!/\d/i.test(args[0])) throw 'Pilih Angka 0 sampai 9 Yang Akan Kamu Pilih!'
+    if (!args[0]) throw 'Por favor, introduzca un número'
+    if (!/\d/i.test(args[0])) throw 'Elije un numero del 0 a 9!'
     const bot = pickRandom(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
     const isWin = bot == args[0]
     conn.reply(m.chat, `
-*「 TEBAK ANGKA 」*
-Angka Kamu : ${args[0]}
-Angka Bot : ${bot}
-Apakah Angkamu Sama Dengan Bot ?
-+${isWin ? bonus : 0} XP!
+– *ADIVINA EL NÚMERO* –
+
+Tu número : ${args[0]}
+Nro del bot : ${bot}
+
+*+${isWin ? bonus : 0}* Exp!
 `.trim(), m)
     if (isWin) global.DATABASE.data.users[m.sender].exp += bonus * 1
 }
-handler.help = ['angka <0-9>']
+handler.help = ['adv <0-9>']
 handler.tags = ['game']
-handler.command = /^angka/i
+handler.command = /^adv/i
 
 module.exports = handler
 
