2e45e8ac68db9ff11467a190d49cf2348fa96194
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-14 14:25:12 -0500

    Add files via upload

--- plugins/mining.js
+++ plugins/mining.js
@@ -45,7 +45,7 @@ handler.disabled = false
 module.exports = handler
 
 function clockString(ms) {
-  let h = Math.floor(ms / 3600000)300000
+  let h = Math.floor(ms / 3600000)
   let m = Math.floor(ms / 60000) % 60
   let s = Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
