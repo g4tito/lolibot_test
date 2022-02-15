0c859dda4465431e671ebc4487e336c98a1316f3
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-15 01:54:42 -0500

    Add files via upload

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -2,9 +2,8 @@ const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
   if (!args[0]) throw 'Ingrese el codigo que se le dio al momento de registrarse'
   let user = global.DATABASE._data.users[m.sender]
-  let snnro = m.sender.split("@")[0]
-  let sn = createHash('md5').update(snnro).digest('hex')
-  if (snnro !== sn) throw 'Codigo incorrecto'
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  if (args[0] !== sn) throw 'Codigo incorrecto'
   user.registered = false
   m.reply(`Se elimino el registro con exito!`)
 }
