5260e0d8d84f9c9d21f308af114fbfdf953613cf
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-15 16:16:33 -0500

    Add files via upload

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,10 +1,9 @@
 let handler = async (m, { usedPrefix, command }) => {
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime)
+let uptime = process.uptime()
 let time = require('moment-timezone').tz('Asia/Kolkata').format('HH:mm:ss')
 let runnya = `
 Hora: ${time}
-Tiempo activa: ${uptime}
+Tiempo activa: ${runtime(uptime)}
 `.trim()
 await m.reply(runnya)
 }
@@ -14,9 +13,15 @@ handler.command = /^(uptime|runtime)$/i
 
 module.exports = handler
 
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
+const runtime = function (seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
