9d5cfa7c95674bbcb6a342d5f92aac289455525e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-15 17:18:00 -0500

    Add files via upload

--- plugins/mining.js
+++ plugins/mining.js
@@ -35,7 +35,7 @@ global.DATABASE._data.users[m.sender].exp += hasiiiil * 1
 - Exp: ${hasiiiil}`)
           }, 0)
           
-            } else m.reply(`Te quedaste sin energía vuelve dentro de *${msToTime(_waktur)}* minutos`)
+            } else m.reply(`Te quedaste sin energía vuelve dentro de *${msToTime(_waktur)}*`)
          } else m.reply(`Sube el nivel a tu pico, escribiendo ${usedPrefix}shop buy pico`)
      } else m.reply(`Todavía no tienes un pico, compralo escribiendo ${usedPrefix}shop buy pico`)
  }
