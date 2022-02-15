422b4cbe658a88960a427e07d1325655efdc5d16
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-15 03:01:33 -0500

    Add files via upload

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -8,16 +8,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('El video debe durar máximo 10 segundos!')
       let img = await q.download()
-      if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
+      if (!img) throw `Responde a un *imagen/video/gif/sticker* con el comando ${usedPrefix + command}`
       let out
       try {
         if (/webp/g.test(mime)) out = await webp2png(img)
         else if (/image/g.test(mime)) out = await uploadImage(img)
         else if (/video/g.test(mime)) out = await uploadFile(img)
         if (typeof out !== 'string') out = await uploadImage(img)
-        stiker = await sticker(false, out, global.packname, global.author)
+        stiker = await sticker(out, false, global.packname, global.author)
       } catch (e) {
         console.error(e)
       } finally {
@@ -25,17 +25,17 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('URL tidak valid!')
+      else return m.reply('Url invalido!')
     }
   } catch (e) {
     console.error(e)
     if (!stiker) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    else throw 'Conversion failed'
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m, contextInfo: {expiration: 604800} })
+    else throw 'La conversión falló'
   }
 }
-handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
+handler.help = ['stiker <imagen/video/gif/sticker>', 'stiker <url>', 'stickergif <imagen/video/gif/sticker>', 'stickergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
