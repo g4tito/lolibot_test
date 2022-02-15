81f94ab9fe17f1f105c33ee41385964f34fdc593
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-15 15:59:40 -0500

    Add files via upload

---
+++ plugins/runtime.js
@@ -0,0 +1,22 @@
+let handler = async (m, { usedPrefix, command }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let time = require('moment-timezone').tz('Asia/Kolkata').format('HH:mm:ss')
+let runnya = `
+Hora: ${time}
+Tiempo activa: ${uptime}
+`.trim()
+await m.reply(runnya, m)
+}
+handler.help = ['runtime']
+handler.tags = ['info']
+handler.command = /^(uptime|runtime)$/i
+
+module.exports = handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
