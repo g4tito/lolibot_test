e51abc40e923765e7e370d38db629c6a00e31a0f
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-16 09:30:21 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -553,19 +553,6 @@ global.dfail = (type, m, conn) => {
   if (msg) return m.reply(msg)
 }
 
-function msToTime(seconds) {
-  d = Math.floor(seconds / (1000 * 60 * 60 * 24));
-  h = Math.floor((seconds / (1000 * 60 * 60)) % 24);
-  m = Math.floor((seconds / (1000 * 60)) % 60);
-  s = Math.floor((seconds / 1000) % 60);
-  
-  dDisplay = d > 0 ? d + (d == 1 ? " dia," : " Dias,") : "";
-  hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
-  mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
-  sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-};
-
 let fs = require('fs')
 let chalk = require('chalk')
 let file = require.resolve(__filename)
