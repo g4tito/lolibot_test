add1c98e792d378810d5703fdae8d4bd4f04fe5e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-18 18:47:13 -0500

    Add files via upload

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,14 +1,17 @@
+let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  if (!text) throw 'Textnya apa?'
-  let res = await fetch(global.API('bg', '/simi', { pesan: text }))
-  let json = await res.json()
-  if (json.status !== true) throw json
-  m.reply(json.result.jawab)
+
+let handler  = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `*Ejemplo de uso:* ${usedPrefix + command} hola simi`
+	fetch("https://api-sv2.simsimi.net/v2/?text=" + encodeURIComponent(text) + "&lang=es")
+  .then(res => res.json())
+  .then(batch => {
+    conn.updatePresence(m.chat, Presence.composing)
+  conn.reply(m.chat, `${batch.success}`, m)
+  }) .catch(() => { conn.reply(m.chat, `Lo siento ocurrió un error :(`, m) })
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' [teks]')
+handler.help = ['simi'].map(v => v + ' <texto>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
-
