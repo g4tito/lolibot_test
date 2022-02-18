13dce3e15c54b1b61786e0043a8cbc9f6117ff53
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-18 17:32:11 -0500

    Add files via upload

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -2,14 +2,44 @@ let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
 const ownernum = "51940617554@s.whatsapp.net"
 let teks = `
-@${ownernum.split("@s.whatsapp.net")[0]}
+Hola @${ownernum.split("@s.whatsapp.net")[0]} ${saludo}
 `.trim()
-conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [ownernum] }})
+conn.sendMessage(m.chat, teks, m, { contextInfo: { mentionedJid: [ownernum] }})
 }
 handler.customPrefix = /Test/
 handler.command = new RegExp
 
 module.exports = handler
+
+var ase = new Date();
+                        var waktoonyabro = ase.getHours();
+                        switch(waktoonyabro){
+                case 0: waktoonyabro = `espero que tengas una linda noche 🌙`; break;
+                case 1: waktoonyabro = `espero que tengas una linda noche 💤`; break;
+                case 2: waktoonyabro = `espero que tengas una linda noche 🦉`; break;
+                case 3: waktoonyabro = `espero que tengas una linda mañana ✨`; break;
+                case 4: waktoonyabro = `espero que tengas una linda mañana 💫`; break;
+                case 5: waktoonyabro = `espero que tengas una linda mañana 🌅`; break;
+                case 6: waktoonyabro = `espero que tengas una linda mañana 🌄`; break;
+                case 7: waktoonyabro = `espero que tengas una linda mañana 🌅`; break;
+                case 8: waktoonyabro = `espero que tengas una linda mañana 💫`; break;
+                case 9: waktoonyabro = `espero que tengas una linda mañana ✨`; break;
+                case 10: waktoonyabro = `espero que tengas un lindo dia 🌞`; break;
+                case 11: waktoonyabro = `espero que tengas un lindo dia 🌨`; break;
+                case 12: waktoonyabro = `espero que tengas un lindo dia ❄`; break;
+                case 13: waktoonyabro = `espero que tengas un lindo dia 🌤`; break;
+                case 14: waktoonyabro = `espero que tengas una linda tarde 🌇`; break;
+                case 15: waktoonyabro = `espero que tengas una linda tarde 🥀`; break;
+                case 16: waktoonyabro = `espero que tengas una linda tarde 🌹`; break;
+                case 17: waktoonyabro = `espero que tengas una linda tarde 🌆`; break;
+                case 18: waktoonyabro = `espero que tengas una linda noche 🌙`; break;
+                case 19: waktoonyabro = `espero que tengas una linda noche 🌃`; break;
+                case 20: waktoonyabro = `espero que tengas una linda noche 🌌`; break;
+                case 21: waktoonyabro = `espero que tengas una linda noche 🌃`; break;
+                case 22: waktoonyabro = `espero que tengas una linda noche 🌙`; break;
+                case 23: waktoonyabro = `espero que tengas una linda noche 🌃`; break;
+            }
+            var saludo = "" + waktoonyabro;
 
 function pickRandom(list) {
     return list[Math.floor(Math.random() * list.length)]
