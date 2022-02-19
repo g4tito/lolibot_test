3df78e5ae3d490ae547796d3dd1068ea00a3ff71
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-19 12:29:01 -0500

    Add files via upload

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,47 +1,24 @@
-const { MessageType, Presence } = require('@adiwajshing/baileys')
+const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
-const uploadFile = require('../lib/uploadFile')
-const uploadImage = require('../lib/uploadImage')
-let { webp2png } = require('../lib/webp2mp4')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler  = async (m, { conn, args }) => {
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('El video debe durar máximo 10 segundos!')
+    if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `Responde a un *imagen/video/gif/sticker* con el comando ${usedPrefix + command}`
-      let out
-      try {
-        if (/webp/g.test(mime)) out = await webp2png(img)
-        else if (/image/g.test(mime)) out = await uploadImage(img)
-        else if (/video/g.test(mime)) out = await uploadFile(img)
-        if (typeof out !== 'string') out = await uploadImage(img)
-        stiker = await sticker(out, false, 'Test', 'xxx')
-      } catch (e) {
-        console.error(e)
-      } finally {
-        if (!stiker) stiker = await sticker(img, false, 'Test', 'xxx')
-      }
-    } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], 'Test', 'xxx')
-      else return m.reply('Url invalido!')
-    }
-  } catch (e) {
-    console.error(e)
-    if (!stiker) stiker = e
-  } finally { 
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
-    else throw 'La conversión falló'
+      if (!img) throw 'Foto/Video tidak ditemukan'
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['stiker <imagen/video/gif/sticker>', 'stiker <url>', 'stickergif <imagen/video/gif/sticker>', 'stickergif <url>']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
-
-const isUrl = (text) => {
-  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
-}
