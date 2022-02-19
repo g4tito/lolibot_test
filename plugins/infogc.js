55ef19483c36e8008ad9e6f15299b03e401ba842
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-18 22:29:04 -0500

    Add files via upload

--- plugins/infogc.js
+++ plugins/infogc.js
@@ -1,50 +1,80 @@
-let handler = async (m, { conn, participants, groupMetadata }) => {
+let handler = async (m, { conn, participants, groupMetadata, text }) => {
+
+    const getGroupAdmins = (participants) => {
+        admins = []
+        for (let i of participants) {
+            i.isAdmin ? admins.push(i.jid) : ''
+        }
+        return admins
+    }
+
     let pp = './src/avatar_contact.png'
     try {
         pp = await conn.getProfilePicture(m.chat)
     } catch (e) {
     } finally {
-        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: Delete } = global.DATABASE._data.chats[m.chat]
-        const groupAdmins = participants.filter(i => i.isAdmin)
-        const groupOwner = groupMetadata.owner || `${m.chat.split`-`[0]}@s.whatsapp.net`
-        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
-        let text = `*「 Group Information 」*\n
-*ID:* 
+        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, expired, descUpdate } = global.db.data.chats[m.chat]
+        const groupAdmins = getGroupAdmins(participants)
+        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split`@`[0]}`).join('\n')
+
+        if (text) return m.reply(msToDate(expired - new Date() * 1))
+
+        let caption = `– *INFORMACION DEL GRUPO* –\n
+• *ID:* 
 ${groupMetadata.id}
-*Name:* 
+
+• *Nombre:* 
 ${groupMetadata.subject}
-*Description:* 
+
+• *Descripción:* 
 ${groupMetadata.desc}
-*Total Members:*
-${participants.length} Members
-*Group Owner:* 
-@${groupOwner.split('@')[0]}
-*Group Admins:*
+
+• *Miembros:*
+${participants.length} total
+
+• *Group Maker:* 
+@${m.chat.split`-`[0]}
+
+• *Administradores/as:*
 ${listAdmin}
-*Group Settings:*
+
+• *Bot Settings:*
 ${isBanned ? '✅' : '❌'} Banned
 ${welcome ? '✅' : '❌'} Welcome
 ${detect ? '✅' : '❌'} Detect
 ${Delete ? '❌' : '✅'} Anti Delete
 ${antiLink ? '✅' : '❌'} Anti Link
-*Message Settings:*
+
+*Bot Message Settings:*
 Welcome: ${sWelcome}
 Bye: ${sBye}
 Promote: ${sPromote}
 Demote: ${sDemote}
+
+*left:*
+${msToDate(expired - new Date() * 1)}
 `.trim()
-        let mentionedJid = groupAdmins.concat([groupOwner])
-        conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, {
-            contextInfo: {
-                mentionedJid
-            }
-        })
+        let mentionedJid = groupAdmins.concat([`${m.chat.split`-`[0]}@s.whatsapp.net`])
+        conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', caption, m, 0, { contextInfo: { mentionedJid } })
     }
 }
-handler.help = ['infogrup']
+handler.help = ['groupinfo']
 handler.tags = ['group']
 handler.command = /^(gro?upinfo|info(gro?up|gc))$/i
 
 handler.group = true
 
 module.exports = handler
+
+function msToDate(ms) {
+    temp = ms
+    days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    daysms = ms % (24 * 60 * 60 * 1000);
+    hours = Math.floor((daysms) / (60 * 60 * 1000));
+    hoursms = ms % (60 * 60 * 1000);
+    minutes = Math.floor((hoursms) / (60 * 1000));
+    minutesms = ms % (60 * 1000);
+    sec = Math.floor((minutesms) / (1000));
+    return days + " Dias " + hours + " Horas " + minutes + " minutos";
+    // +minutes+":"+sec;
+}
