2639fd28eed089941e05f5a48d874c1a8b39f2cb
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-23 22:41:55 -0500

    Add files via upload

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,14 +1,14 @@
-let { performance } = require('perf_hooks')
+let speed = require('performance-now')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, DevMode }) => {
     try {
-        let old = performance.now()
-        await m.reply('_Testing speed..._')
+        let timestamp = speed()
         let neww = performance.now()
-        m.reply(neww - old + 'ms')
+        let latensi = speed() - timestamp
+        m.reply(`🚀 *Velocidad:* ${latensi.toFixed(4)} _Segundos_`)
     } catch (e) {
         console.log(e)
-        m.reply('Error!!')
+        m.reply('Lo siento ocurrio un error')
         if (DevMode) {
             for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
                 conn.sendMessage(jid, 'Speed.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
