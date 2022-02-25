7bcd2d29db163cde51f86182e3c08759669b8b3d
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-25 14:12:27 -0500

    Add files via upload

--- plugins/play.js
+++ plugins/play.js
@@ -1,53 +1,42 @@
-let limit = 30
-let { MessageType } = require('@adiwajshing/baileys')
-let yts = require('yt-search')
 const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, usedPrefix, command, text, isPrems, isOwner, DevMode }) => {
-  conn.play = conn.play ? conn.play : {}
-  if (m.chat in conn.play) throw 'Todavía hay quienes están buscando en youtube en este chat... esperen a que termine'
-  else conn.play[m.chat] = true
-  try {
-      try {
-          if (!text) throw `*Ingrese el nombre de la música*\n\n- Ejemplo : ${usedPrefix + command} beliver`
-          conn.reply(m.chat, wait, m) 
-          let results = await yts(text)
-          let vid = results.all.find(video => video.seconds < 3600)
-          if (!vid) throw 'No se encontró edeo/Audio'
-          let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
-          let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-          conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*──「 Descarga de Youtube 」──*
-
-• *🎵 Titulo* : ${title}
-• *🔊 Tamaño* : ${filesizeF}
-• *👾 Link* : ${vid.url}
-
-${isLimit ? 'El archivo es muy grande descargalo por el link\n': ''}- Link de descarga : _${dl_link}_
-`.trim(), m)
-          if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Video descargado*
-
-• *📽️ Titulo* : ${title}
-`.trim(), m)
-      } catch (e) {
-          console.log(e)
-          m.reply('error')
-          if (DevMode) {
-              for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                  conn.sendMessage(jid, 'Play.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
-              }
-          }
-      }
-  } finally {
-    delete conn.play[m.chat]
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `*Ingrese el nombre de la musica*\n\n- Ejemplo: ${usedPrefix + command} beliver`
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw 'No se encontro el video/audio'
+  let yt = false
+  let yt2 = false
+  let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nPrueba con otro servidor...'}`)
+    }
   }
+  if (yt === false) throw 'Todos los servidores fallaron'
+  if (yt2 === false) throw 'Todos los servidores fallaron'
+  let { dl_link, thumb, title, filesize, filesizeF } = yt
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+- *Judul:* ${title}
+- *Ukuran File Audio:* ${filesizeF}
+- *Ukuran File Video:* ${yt2.filesizeF}
+- *Server y2mate:* ${usedServer}
+`.trim(), footer, 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`, m)
 }
-handler.help = ['play', 'play2'].map(v => v + ' <titulo>')
+handler.help = ['play', 'play2']
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
+handler.command = /^(play|play2)$/i
 
 handler.exp = 0
-handler.limit = false
 
 module.exports = handler
+
+
 
