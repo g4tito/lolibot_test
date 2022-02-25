162a31eb49a9eef547513696ba08951687062991
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-25 18:13:10 -0500

    Add files via upload

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,26 +1,30 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
+let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner }) => {
+  if (!args || !args[0]) return m.reply(`*Introdusca un link de una música de youtube*\n\n- Ejemplo:  ${usedPrefix + command} https://youtu.be/5DH20WZenDA`)
   let chat = global.DATABASE._data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*< + DESCARGA DE AUDIO +/>*
+
+- 📝Título: ${title}
+- 📦Tamaño: ${filesizeF}
+
+*Link de descarga:*
+_${dl_link}_
 `.trim(), m)
+  if (isLimit) return m.reply('El audio es muy largo, la descarga se cancelo')
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+*${title}*
 `.trim(), m, null, {
   asDocument: chat.useDocument
 })
 }
-handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.help = ['yta','mp3'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^yt(a|mp3)$/i
+handler.command = /^(yta|mp3)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,31 +1,35 @@
 let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
+let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner }) => {
+  if (!args || !args[0]) return m.reply(`*Introdusca un link de un video de youtube*\n\n- Ejemplo:  ${usedPrefix + command} https://youtu.be/5DH20WZenDA`)
   let chat = global.DATABASE._data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*< + DESCARGA DE VIDEO +/>*
+
+- 📝Título: ${title}
+- 📦Tamaño: ${filesizeF}
+
+*Link de descarga:*
+_${dl_link}_
 `.trim(), m)
+  if (isLimit) return m.reply('El video es muy largo, la descarga se cancelo')
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+*${title}*
 `.trim(), m, false, {
   ..._thumb,
   asDocument: chat.useDocument
 })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.help = ['ytv','mp4'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
+handler.command = /^(ytv|mp4)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
