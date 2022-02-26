931fb3054e90a8c7baf5f70351bcb358c81961b6
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-25 22:14:38 -0500

    Add files via upload

--- plugins/ytvbutton.js
+++ plugins/ytvbutton.js
@@ -3,12 +3,12 @@ let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner }) => {
   if (!args || !args[0]) return m.reply('Error, link inválido')
+  m.reply('↓ ‧ _Descargando video, espere.._')
   let chat = global.DATABASE._data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   if (isLimit) return m.reply('El video es muy largo, la descarga se cancelo')
-  m.reply('↓ ‧ _Descargando video, espere.._')
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
