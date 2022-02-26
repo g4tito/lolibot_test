941773b0c5a14d73e794cebc319870ec4058ebf2
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-25 22:16:22 -0500

    Add files via upload

--- plugins/ytabutton.js
+++ plugins/ytabutton.js
@@ -2,12 +2,12 @@ let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner }) => {
   if (!args || !args[0]) return m.reply('Error, link invalido')
+  m.reply('↓ ‧ _Descargando audio, espere.._')
   let chat = global.DATABASE._data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   if (isLimit) return m.reply('El audio es muy largo, la descarga se cancelo')
-  m.reply('↓ ‧ _Descargando audio, espere.._')
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
 *${title}*
 `.trim(), m, null, {
