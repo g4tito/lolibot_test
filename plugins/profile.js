3a5e37be49beac5a098f3fe0849d43d31c551b79
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-27 03:11:19 -0500

    Add files via upload

--- plugins/profile.js
+++ plugins/profile.js
@@ -14,9 +14,9 @@ let handler = async (m, { conn, usedPrefix }) => {
     let username = conn.getName(who)
     let math = max - xp
     let prem = global.prems.includes(who.split`@`[0])
-    let str = `\t\t\t\t\t\t･ 【 *PERFIL INFO* 】 ･
+    let str = `\t\t\t\t\t･ 【 *PERFIL INFO* 】 ･
 
-• Nombre: ${username}${registered ? '\n• Nombre de Reg : ' + name : ''}\n• Tag: @${who.replace(/@.+/, '')}${about ? '\n• Estado : ' + about : ''}
+• Nombre: ${username} ${registered ? '\n• Nombre de Reg : ' + name : ''}\n• Tag: @${who.replace(/@.+/, '')}${about ? '\n• Estado : ' + about : ''}
 • Nunero: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 • Link: https://wa.me/${who.split`@`[0]}${registered ? '\n• *Edad* : ' + age : ''}
 • Exp: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Listo para subir de nivel *${usedPrefix}levelup*` : `${math} Exp restante para subir de nivel`}]
