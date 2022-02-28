dbdebd217bc638fd2b513ea5d9371cd6a927200c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-28 17:53:20 -0500

    Add files via upload

--- plugins/Internet.js
+++ plugins/Internet.js
@@ -3,9 +3,9 @@ let isOnline = require('is-online');
  let handler  = async (m, { command, conn, text }) => {
 
 (async () => {
-	m.reply(await isOnline());
+	let net = await isOnline()
 })();
-
+m.reply(`${net}`)
 }
 handler.help = ['internet']
 handler.tags = ['info']
