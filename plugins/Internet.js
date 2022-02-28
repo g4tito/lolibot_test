40be3fd3b6c9e1ba78b62961904106f34f9104b6
g4tito <84881966+g4tito@users.noreply.github.com>
2022-02-28 17:47:02 -0500

    Add files via upload

--- plugins/Internet.js
+++ plugins/Internet.js
@@ -1,14 +1,10 @@
-var internetAvailable = require("internet-available");
+let isOnline = require('is-online');
 
  let handler  = async (m, { command, conn, text }) => {
-internetAvailable({
-    timeout: 5000,
-    retries: 5
-}).then(() => {
-    m.reply("✅ Con internet");
-}).catch(() => {
-    m.reply("❎ Sin internet");
-});
+
+(async () => {
+	console.log(await isOnline());
+})();
 
 }
 handler.help = ['internet']
