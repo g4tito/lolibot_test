5e6df03553e188c81f124807fdf4dc1e9334f321
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-05 19:46:32 -0500

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
+  conn.reply(m.chat, wait, m)
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Lo siento ocurrio un error!'
