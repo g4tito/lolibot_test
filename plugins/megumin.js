844e2e45e4cec8da3c38f6a44e06f94ae05a5cc2
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-07 12:14:25 -0500

    Update megumin.js

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/megumin')
+  conn.reply(m.chat, wait, m)
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.url) throw 'Lo siento ocurrio un error!'
@@ -8,6 +9,6 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['megumin']
 handler.tags = ['random']
-handler.command = /^megumin$/i
+handler.command = /^(megumin)$/i
 
 module.exports = handler
