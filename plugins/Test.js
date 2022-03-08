7f37dee9bf701e991f717d4d355a4b86592b23ff
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-08 18:27:43 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,41 +1,8 @@
-let fs = require ('fs')
+let fs = require ('fs');
+let { MessageType } = require("@adiwajshing/baileys");
 let handler = async (m, { conn, text }) => {
-	/*let tumb = fs.readFileSync('./src/menu2.jpg')
-    let list = await conn.prepareMessageFromContent(m.chat, {
-    "listMessage": {
-      "title": "TEST PRODUCT",
-      "description": "Test",
-      "buttonText": "",
-      "listType": "PRODUCT_LIST",
-      "productListInfo": {
-        "productSections": [
-          {
-            "title": "Test_2",
-            "products": [
-              {
-                 "productId": "4120139628109348"
-              },
-              {
-              	"productId": "6431678466857362"
-              },
-              {
-              	"productId": "4392524570816732"
-              }
-            ]
-          }
-        ],
-        "headerImage": {
-          "productId": "4120139628109348",
-          "productId": "6431678466857362",
-          "productId": "4392524570816732",
-          "jpegThumbnail": tumb
-        },
-        "businessOwnerJid": "51940617554@s.whatsapp.net"
-      },
-      "footerText": "\nPowered by Gatito"
-    }
-  }, {quoted: m })
-  conn.relayWAMessage(list, { waitForAck: true })*\
+let tumb = fs.readFileSync('./src/menu2.jpg')
+conn.sendMessage(m.chat, `Test`, MessageType.SendPaymentMessage)
 }
 
 handler.customPrefix = /Test/i
