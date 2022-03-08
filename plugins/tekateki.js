36e13e3d44e4e4dad07a4f23d5aa493a9bc0fa1a
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-08 02:10:24 -0500

    Update tekateki.js

--- plugins/tekateki.js
+++ plugins/tekateki.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tekateki[id][0])
+        conn.reply(m.chat, 'Todavía hay preguntas sin responder en este chat', conn.tekateki[id][0])
         throw false
     }
     if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tekateki.json'))).json()
@@ -15,21 +15,21 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (!json) throw json
     let caption = ` 
 *${json.soal.trim()}?* 
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hah untuk bantuan
-Bonus: ${poin} XP
+Tiempo : ${(timeout / 1000).toFixed(2)} segundos
+Escriba *${usedPrefix}hah* para obtener ayuda
+Bono de respuesta correcta : ${poin} Exp
 `.trim()
     conn.tekateki[id] = [
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tekateki[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) conn.reply(m.chat, `¡Se acabó el tiempo!\nLa respuesta es : *${json.answer}*`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
 }
 handler.help = ['tekateki']
 handler.tags = ['game']
-handler.command = /^tekateki/i
+handler.command = /^(adivinanza|tekateki)$/i
 
 module.exports = handler
