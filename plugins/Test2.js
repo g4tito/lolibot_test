ecd8f906c8be952a65bb1356ad384095a264a40b
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-09 14:52:18 -0500

    Create Test2.js

---
+++ plugins/Test2.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+if (budy == `${prefix + command}`){
+conn.sendMessage(from, 'Teest', text)
+}
+
+}
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
