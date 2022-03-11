3adda55042e4e287e684ae58f045745e27e80a7a
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-10 23:15:04 -0500

    Update use.js

--- plugins/use.js
+++ plugins/use.js
@@ -10,7 +10,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
         if (/use|pakai/i.test(command)) {
             try {
                 let count = (/[0-9]/g.test(args[1])) ? !args[1] || args.length < 2 ? Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1) : Math.max(args[1], 1) : Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1)
-                 let msgsucces = (pickRandom(['Acabas de beber', 'Bebiste cum digo,', 'Bebiste', 'Tu usas', 'Acabas de usar']) + ' *' + (count * 1) + '* pocion')
+                 let msgsucces = (pickRandom(['Acabas de beber', 'Bebiste cum digo,', 'Glu glu glu... bebiste', 'Tu usas', 'Acabas de usar']) + ' *${10 <= 1 ? 'una pocion' : `${(count * 1)} pociones`}*')
                  if (args[0] === 'pocion') {
                     if (global.DATABASE._data.users[m.sender].healt < 100) {
                         if (global.DATABASE._data.users[m.sender].potion >= count * 1) {
@@ -19,7 +19,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
                             conn.reply(m.chat, msgsucces, m)
                         } else conn.reply(m.chat, msgkurang, m)
                     } else conn.reply(m.chat, msgpenuh, m)
-                } else if (args.length > 2 && args[0] === !'potion') m.reply(pickRandom(['Solo puede usar pociones.', 'Mau ngunain apa? Cuma bisa gunain potion :v', 'Wow, que quieres usar, solo puedes hacer pociones', 'Waduheck, hanya bisa potion', 'lah, mau gunain apa?, kan hanya bisa potion']) + '\nContoh penggunaan: *' + usedPrefix + 'potion 1*')
+                } else if (args.length > 2 && args[0] === !'pocion') m.reply(pickRandom(['Solo puede usar pociones.', 'Mau ngunain apa? Cuma bisa gunain potion :v', 'Wow, que quieres usar, solo puedes hacer pociones', 'Waduheck, hanya bisa potion', 'lah, mau gunain apa?, kan hanya bisa potion']) + '\nContoh penggunaan: *' + usedPrefix + 'potion 1*')
             } catch (e) {
                 console.log(e)
                 m.reply(msgerror)
