cc4de7679c599d757f3b5e8fc5c79eb88b615928
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-11 17:47:34 -0500

    Update delmsg.js

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -2,11 +2,11 @@ let handler = async (m, { command, usedPrefix, text }) => {
     let which = command.replace(/get/i, '')
     if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
     let msgs = global.DATABASE._data.msgs
-    if (!text in msgs) throw `'${text}' tidak terdaftar di list pesan`
+    if (!text in msgs) throw `'${text}' no está en la lista de mensajes`
     delete msgs[text]
-    m.reply(`Berhasil menghapus pesan di list pesan dengan nama '${text}'`)
+    m.reply(`Se eliminó el mensaje con el nombre '${text}' de la lista`)
 }
-handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <teks>')
+handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'del' + v + '')
 handler.tags = ['database']
 handler.command = /^(-|del)(vn|msg|video|audio|img|stic?ker|gif)$/
 
