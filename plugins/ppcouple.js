80df4e752af4bc5a4cc7e82763006356b93fe17c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-11 00:22:51 -0500

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -5,15 +5,16 @@ let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
   let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
+  conn.reply(m.chat, wait, m)
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'Nih PP Yg Cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'Dan Ni PP YgCewe', m)
+  await conn.sendFile(m.chat, json.result.male, '', '*✅ Chico*', m)
+  await conn.sendFile(m.chat, json.result.female, '', '*✅ Chica*', m)
 }
-handler.help = ['ppcouple', 'ppcp']
+handler.help = ['par']
 handler.tags = ['internet']
-handler.command = /^(pp(cp|couple))$/i
+handler.command = /^(par)$/i
 handler.limit = true
 
 module.exports = handler
