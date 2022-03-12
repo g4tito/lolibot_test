f57528a1b974d8e6f6a9eb37130d08caeee48ee8
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-12 18:16:47 -0500

    Create pinterestvideo.js

---
+++ plugins/pinterestvideo.js
@@ -0,0 +1,24 @@
+const { pin } = require('../lib/scrape')
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+    if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
+    if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `Link invalido!`
+
+    pin(args[0]).then(async res => {
+        let pin = JSON.stringify(res)
+        let json = JSON.parse(pin)
+        if (!json.status) throw `Tidak dapat diunduh`
+        conn.reply(m.chat, wait, m)
+        await conn.sendFile(m.chat, json.data.url, '', kasihcaption, m) //, false )
+
+    })
+
+}
+handler.help = ['pinterestvideo']
+handler.tags = ['downloader']
+handler.command = /^(pinterestvideo|pvideo)$/i
+
+handler.limit = true
+
+module.exports = handler
