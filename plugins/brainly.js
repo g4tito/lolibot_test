54e654a81b73a9c7087a6bdb2b0ab2f37209b0dd
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-12 00:19:46 -0500

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,17 +1,13 @@
-let { Brainly } = require("brainly-scraper-v2").default;
-let brain = new Brainly("id");
+const brainly = require('brainly-scraper-v2')
 let handler = async function (m, { text, usedPrefix, command }) {
-  if (!text) throw `uhm.. soalnya mana?\n\ncontoh:\n${usedPrefix + command} apa itu javascript?`
-  brain.search("id", text).then(async res => {
-    let br = JSON.stringify(res)
-    let json = JSON.parse(br)
-    let answer = json.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.question.content}\n${v.answers.map((v, i) => `*JAWABAN KE ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•------------•\n\n')
-    m.reply(answer)
-  })
+  if (!text) throw 'Because?'
+  let res = await brainly(text)
+  let answer = res.data.map((v, i) => `_*QUESTION TO ${i + 1}*_\n${v.question}\n${v.answer.map((v,i) => `*ANSWER TO ${i + 1}*\n$ {v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
+  m.reply(answer)
 }
-handler.help = ['brainly']
+handler.help = ['brainly <soal>']
 handler.tags = ['internet']
 
-handler.command = /^(brainly)$/i
+handler.command = /^brainly$/i
 
 module.exports = handler
