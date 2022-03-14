4b8c981295d4706dbcc3a78b90b571bb4b5b4c97
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-13 21:25:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,67 +3,67 @@ apt update && apt upgrade
 bin=$PREFIX/bin
 dir=$(pwd)
 if [ -e $bin/node ]; then 
-  echo -e "\u001b[32mSkip install nodejs but already installed!"
+  echo -e "\u001b[32mYa estaba instalado nodejs!"
 else 
-  echo -e "\u001b[31mInstaling \u001b[31mnodejs!"
+  echo -e "\u001b[31mInstalando... \u001b[31mnodejs!"
   pkg install nodejs -y
-  echo -e "\u001b[32mDone install nodejs!"
+  echo -e "\u001b[32mSe instaló nodejs!"
 fi
 
 if [ -e $bin/ffmpeg ]; then 
-  echo -e "\u001b[32mSkip install ffmpeg but already installed!"
+  echo -e "\u001b[32mYa esta instalado ffmpeg!"
 else
-  echo -e "\u001b[31mInstaling \u001b[31mffmpeg!"
+  echo -e "\u001b[31mInstalando... \u001b[31mffmpeg!"
   pkg install ffmpeg -y
-  echo -e "\u001b[32mDone install ffmpeg!"
+  echo -e "\u001b[32mSe instaló ffmpeg!"
 fi
 
 if [ -e $bin/magick ]; then 
-  echo -e "\u001b[32mSkip install imagemagick but already installed!"
+  echo -e "\u001b[32mYa esta instalado imagemagick!"
 else
-  echo -e "\u001b[31mInstaling \u001b[31mimagemagick!"
+  echo -e "\u001b[31mInstalando... \u001b[31mimagemagick!"
   pkg install imagemagick -y
-  echo -e "\u001b[32mDone install imagemagick!"
+  echo -e "\u001b[32mSe instaló imagemagick!"
 fi
 
-echo -e "\u001b[32mUpdate npm version"
+echo -e "\u001b[32mActualizando la versión de npm"
 npm install -g npm@6.14.14
-echo -e "\u001b[32mDone Update npm version!"
+echo -e "\u001b[32mSe actualizó la versión de npm!"
 
-echo -e "\u001b[33mChecking instalation!"
+echo -e "\u001b[33mComprobando instalación!"
 check() {
   if [ -e $bin/node ]; then
-    echo -e "\u001b[32mNodejs already installed!"
+    echo -e "\u001b[32mNodejs ya está instalado!"
   else
-    echo -e "\u001b[31mNodejs not installed!!, install manualy using `pkg install nodejs -y` or `apt install nodejs -y`"
+    echo -e "\u001b[31mNodejs no esta instalado!!, instalada manualmente usando `pkg install nodejs -y` o `apt install nodejs -y`"
     exit 1
   fi 
 
   if [ -e $bin/ffmpeg ]; then
-    echo -e "\u001b[32mFfmpeg already installed!" 
+    echo -e "\u001b[32mFfmpeg ya está instalado!" 
   else
-    echo -e "\u001b[31mFfmpeg not installed!!, install manualy using `pkg install ffmpeg -y` or `apt install ffmpeg -y`"
+    echo -e "\u001b[31mFfmpeg no esta instalado!!, instalada manualmente usando `pkg install ffmpeg -y` o `apt install ffmpeg -y`"
   fi 
 
   if [ -e $bin/magick ]; then
-    echo -e "\u001b[32mImagemagick already installed!" 
+    echo -e "\u001b[32mImagemagick ya está instalado!" 
   else
-    echo -e "\u001b[31mImagemagick not installed!!, install manualy using `pkg install imagemagick -y` or `apt install imagemagick -y`"
+    echo -e "\u001b[31mImagemagick no esta instalado!!, instalada manualmente usando `pkg install imagemagick -y` o `apt install imagemagick -y`"
   fi 
 }
 
 check
-echo -e "\u001b[32mDone checking instalation!" 
+echo -e "\u001b[32mSe revisó todas las instalaciones!" 
 
-echo -e "\u001b[33mInstall and update module!"
+echo -e "\u001b[33mInstalando y actualizando el módulo!"
 if [ -e $dir/package.json ]; then
   npm install
   npm update
-  echo -e "\u001b[32Done Install and update module!" 
+  echo -e "\u001b[32Se instaló y actualizó el módulo" 
 else 
-  echo -e "\u001b[31mPackage.json not found!"
+  echo -e "\u001b[31mError package.json!"
 fi 
 
-echo -e "\u001b[32mStarting bot..."
-echo -e "\u001b[32mIf found bug pls report in \u001b[33;1mhttps://github.com/Nurutomo/wabot-aq/issues/new?assignees=&labels=&template=bug_report.md&title="
+echo -e "\u001b[32mIniciando lolibot..."
+echo -e "\u001b[32mSi encuentra un error, por favor informe en \u001b[33;1mhttps://github.com/g4tito/lolibot/issues/new?assignees=&labels=&template=bug_report.md&title="
 npm run start
