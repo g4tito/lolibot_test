8a33d554287e0e7fbc1e5e70bc08027151d4e589
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-14 19:54:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,70 +1,22 @@
-#!bin/bash
+spin () {
 
-bin=$PREFIX/bin
-dir=$(pwd)
-if [ -e $bin/node ]; then 
-  echo -e "\u001b[32mYa estaba instalado nodejs!"
-else 
-  echo -e "\u001b[31mInstalando... \u001b[31mnodejs!"
-  pkg install nodejs -y
-  echo -e "\u001b[32mSe instaló nodejs!"
-fi
+local pid=$!
+local delay=0.25
+local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
 
-if [ -e $bin/ffmpeg ]; then 
-  echo -e "\u001b[32mYa esta instalado ffmpeg!"
-else
-  echo -e "\u001b[31mInstalando... \u001b[31mffmpeg!"
-  pkg install ffmpeg -y
-  echo -e "\u001b[32mSe instaló ffmpeg!"
-fi
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
 
-if [ -e $bin/magick ]; then 
-  echo -e "\u001b[32mYa esta instalado imagemagick!"
-else
-  echo -e "\u001b[31mInstalando... \u001b[31mimagemagick!"
-  pkg install imagemagick -y
-  echo -e "\u001b[32mSe instaló imagemagick!"
-fi
+for i in "${spinner[@]}"
+do
+	tput civis
+	echo -ne "\033[34m\r[*] Downloading..please wait.........\e[33m[\033[32m$i\033[33m]\033[0m   ";
+	sleep $delay
+	printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+tput cnorm
+printf "\e[1;33m [Done]\e[0m";
+echo "";
 
-echo -e "\u001b[32mActualizando la versión de npm"
-npm install -g npm@6.14.14
-echo -e "\u001b[32mSe actualizó la versión de npm!"
-
-echo -e "\u001b[33mComprobando instalación!"
-check() {
-  if [ -e $bin/node ]; then
-    echo -e "\u001b[32mNodejs ya está instalado!"
-  else
-    echo -e "\u001b[31mNodejs no esta instalado!!, instalada manualmente usando `pkg install nodejs -y` o `apt install nodejs -y`"
-    exit 1
-  fi 
-
-  if [ -e $bin/ffmpeg ]; then
-    echo -e "\u001b[32mFfmpeg ya está instalado!" 
-  else
-    echo -e "\u001b[31mFfmpeg no esta instalado!!, instalada manualmente usando `pkg install ffmpeg -y` o `apt install ffmpeg -y`"
-  fi 
-
-  if [ -e $bin/magick ]; then
-    echo -e "\u001b[32mImagemagick ya está instalado!" 
-  else
-    echo -e "\u001b[31mImagemagick no esta instalado!!, instalada manualmente usando `pkg install imagemagick -y` o `apt install imagemagick -y`"
-  fi 
 }
-
-check
-echo -e "\u001b[32mSe revisó todas las instalaciones!" 
-
-echo -e "\u001b[33mInstalando y actualizando el módulo!"
-if [ -e $dir/package.json ]; then
-  npm install
-  npm update
-  echo -e "\u001b[32Se instaló y actualizó el módulo" 
-else 
-  echo -e "\u001b[31mError package.json!"
-fi 
-
-echo -e "\u001b[32mIniciando lolibot..."
-echo -e "\u001b[32mSi encuentra un error, por favor informe en \u001b[33;1mhttps://github.com/g4tito/lolibot/issues/new?assignees=&labels=&template=bug_report.md&title="
-npm start
-
